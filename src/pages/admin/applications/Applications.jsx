9e228a157c16de7c9b27852ebe6cf1c30025e7a0
Dilshod Mahmdov <dilshodengineer@gmail.com>
2026-07-15 14:14:59 +0500

    add component Applications and its table

--- src/pages/admin/applications/Applications.jsx
+++ src/pages/admin/applications/Applications.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Container from '../../../components/layouts/admin/Container'
-import { Link } from 'react-router'
+import React from 'react';
+import Container from '../../../components/layouts/admin/Container';
+import ApplicationsTable from '../../../components/tables/ApplicationsTable';
 
 const Applications = () => {
   return (
@@ -8,71 +8,9 @@ const Applications = () => {
 
       <h4>Arizalar</h4>
 
-      <table className='table align-middle'>
-        <thead>
-
-          <tr>
-            <th>#</th>
-            <th>Ismi</th>
-            <th>Kurs</th>
-            <th>Tarif</th>
-            <th>Boshqarish</th>
-          </tr>
-
-        </thead>
-        <tbody>
-
-          <tr>
-            <td>1</td>
-            <td>
-              <span>Dilshod Mahmudov</span>
-              <br />
-              <span className='small text-secondary'>dilshod@gmail.com</span>
-            </td>
-            <td>Laravel</td>
-            <td>
-              <b>Pro-new</b>
-              <br />
-              <span className="text-success">
-                25000
-              </span> so'm
-            </td>
-            <td>
-              <form className="d-flex gap-3">
-                <Link to="/" className='text-secondary'>Chekini ko'rish</Link>
-                <input type="date" className='btn btn-secondary py-0' />
-                <button className="btn btn-sm btn-primary">Kursni ochish</button>
-              </form>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>
-              <span>Sunnatilla Vahobjonov</span>
-              <br />
-              <span className='small text-secondary'>sunnatilla@gmail.com</span>
-            </td>
-            <td>Matematika</td>
-            <td>
-              <b>Pro</b>
-              <br />
-              <span className="text-success">
-                300000
-              </span> so'm
-            </td>
-            <td>
-              <form className="d-flex gap-3">
-                <Link to="/" className='text-secondary'>Chekini ko'rish</Link>
-                <input type="date" className='btn btn-secondary py-0' />
-                <button className="btn btn-sm btn-primary">Kursni ochish</button>
-              </form>
-            </td>
-          </tr>
-
-        </tbody>
-      </table>
+      <ApplicationsTable/>
     </Container>
-  )
-}
+  );
+};
 
-export default Applications
+export default Applications;
